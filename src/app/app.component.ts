d348eedb6dacf9094bb98464c924e8ad1320de57
Andrew <andrew.morgan@e.org>
2022-03-15 20:53:27 +0000

    added generic components and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    this.moviesInTheaters = [
+      {
+        title: 'spider-man',
+        releaseDate: new Date('2010-08-15'),
+        price: 8.99,
+      },
+      { title: 'Moana', releaseDate: new Date('2016-11-14'), price: 14.99 },
+    ];
+    this.moviesFutureReleases = [
+      {
+        title: 'Avengers',
+        releaseDate: new Date('2011-03-22'),
+        price: 12.99,
+      },
+      {
+        title: 'Jurassic Park',
+        releaseDate: new Date('1994-11-14'),
+        price: 3.99,
+      },
+    ];
+  }
+
   title = 'angular-movie-library';
+  moviesInTheaters: any;
+  moviesFutureReleases: any;
 }
