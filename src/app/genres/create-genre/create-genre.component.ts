6c71498806e50dcd70342ace86bb1fb5aeabe676
Andrew <andrew.morgan@e.org>
2022-03-17 23:56:04 +0000

    added custom validator

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,5 +1,6 @@
+import { firstLetterUppercase } from 'src/app/validators/firstLetterUppercase';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -14,11 +15,34 @@ export class CreateGenreComponent implements OnInit {
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      name: '',
+      name: [
+        '',
+        {
+          validators: [
+            Validators.required,
+            Validators.minLength(3),
+            firstLetterUppercase(),
+          ],
+        },
+      ],
     });
   }
 
   saveChanges() {
     this.router.navigate(['/genres']);
+  }
+
+  getErrorMessageFieldName() {
+    const field = this.form.get('name');
+    if (field?.hasError('required')) {
+      return 'The name field is required';
+    }
+    if (field?.hasError('minlength')) {
+      return 'The name must be at least 3 characters long';
+    }
+    if (field?.hasError('firstLetterUppercase')) {
+      return field.getError('firstLetterUppercase').message;
+    }
+    return '';
   }
 }
