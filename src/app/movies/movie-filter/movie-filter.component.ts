771e48090bc47e78744226a839140cb97dc974db
Andrew <andrew.morgan@e.org>
2022-03-18 14:32:48 +0000

    implemented title filtering

--- src/app/movies/movie-filter/movie-filter.component.ts
+++ src/app/movies/movie-filter/movie-filter.component.ts
@@ -1,5 +1,6 @@
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-movie-filter',
@@ -18,6 +19,26 @@ export class MovieFilterComponent implements OnInit {
     { id: 4, name: 'Crime' },
   ];
 
+  movies = [
+    {
+      title: 'Spider Man',
+      poster:
+        'https://www.themoviedb.org/t/p/original/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    },
+    {
+      title: 'Moana',
+      poster:
+        'https://www.themoviedb.org/t/p/original/6iHQpFiTg0QbKYac5Mprhx7tXo3.jpg',
+    },
+    {
+      title: 'Inception',
+      poster:
+        'https://cdn.shopify.com/s/files/1/1416/8662/products/inception_2010_french_original_film_art_399c9bc0-2060-432c-a24b-873162f00abd_600x.jpg?v=1643315917',
+    },
+  ];
+
+  originalMovies = this.movies;
+
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       title: '',
@@ -25,7 +46,22 @@ export class MovieFilterComponent implements OnInit {
       upcomingReleases: false,
       inTheaters: false,
     });
+
+    this.form.valueChanges.subscribe((values) => {
+      this.movies = this.originalMovies;
+      this.filterMovies(values);
+    });
   }
 
-  clearForm() {}
+  filterMovies(values: any) {
+    if (values.title) {
+      this.movies = this.movies.filter(
+        (movie) => movie.title.indexOf(values.title) !== -1
+      );
+    }
+  }
+
+  clearForm() {
+    this.form.reset();
+  }
 }
