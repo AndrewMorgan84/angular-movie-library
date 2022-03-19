1646c489a19197e31e15e954f84f5cabaee6a05c
Andrew <andrew.morgan@e.org>
2022-03-19 21:12:52 +0000

    added map component and leaflet packages

---
+++ src/app/utilities/map/map.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+import { latLng, tileLayer, LeafletMouseEvent, Marker, marker } from 'leaflet';
+
+const iconRetinaUrl = './assets/marker-icon-2x.png';
+const iconUrl = './assets/marker-icon.png';
+const shadowUrl = './assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
+L.Marker.prototype.options.icon = iconDefault;
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css'],
+})
+export class MapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Angular Movies',
+      }),
+    ],
+    zoom: 16,
+    center: latLng(50.722163462381815, -3.5372352699050684),
+  };
+
+  layers: Marker<any>[] = [];
+
+  handleMapClick(event: LeafletMouseEvent) {
+    const latitude = event.latlng.lat;
+    const longitude = event.latlng.lng;
+    console.log({ latitude, longitude });
+    this.layers = [];
+    this.layers.push(marker([latitude, longitude]));
+  }
+}
